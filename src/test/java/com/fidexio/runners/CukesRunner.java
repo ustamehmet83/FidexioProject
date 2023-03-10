f08cddcc333045c730c32bfaad16714080094505
Mehmet Usta <ustamehmet83@hotmail.com>
2023-03-10 12:20:56 +0300

    First Use Case Automate

--- src/test/java/com/fidexio/runners/CukesRunner.java
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+
         },
        features = "src/test/resources/features",
         glue = "com/fidexio/step_definitions",
